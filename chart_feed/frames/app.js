6fa4cbeca19e7404ea7d01f1cf067b27d5128d25
cryptocodyclub <jtchaschowy@gmail.com>
2018-03-16 04:48:58 -0700

    update

--- chart_feed/frames/app.js
+++ chart_feed/frames/app.js
@@ -36,7 +36,7 @@ function scrollDown(el){
             if (self.times == 1200) {
                 return self.moveBack();
             }
-        }, 125);
+        }, 100);
     }
 
     this.startMove = function() {
